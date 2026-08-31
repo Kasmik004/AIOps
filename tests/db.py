8e80f16fa7c2d171807dae14a45b6a7fba46499c
Kasmik004 <67088222+Kasmik004@users.noreply.github.com>
2026-08-31 22:05:55 +0545

    Modify DATABASE_URL to prioritize env variable
    Update DATABASE_URL to use environment variable.

--- tests/db.py
+++ tests/db.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 DATABASE_URL = (
     os.getenv("DATABASE_URL")
-    or "postgresql://neondb_owner:npg_XvLrKc3TyRf7@ep-noisy-math-azvrgb2d.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
 )
 
 with psycopg.connect(DATABASE_URL) as conn:
